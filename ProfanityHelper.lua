9ef2ddd91294ec762f60f6815325492143058605
tonic <tonicbupt@gmail.com>
2021-06-11 12:43:13 +0800

    bump version to support 1.3.7, 2.5.1, 9.0.5

---
+++ ProfanityHelper.lua
@@ -0,0 +1,6 @@
+-- really need this...
+if GetLocale() == "zhCN" then
+    ConsoleExec('SET portal "TW"')
+    ConsoleExec('SET profanityFilter "0"')
+    ConsoleExec('SET overrideArchive "0"')
+end
